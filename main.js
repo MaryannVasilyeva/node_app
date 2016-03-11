4fdde72edb69369348ad00c2f98c48b5150067ad
MaryannVasilyeva <marvas987@gmail.com>
2016-03-10 18:36:23 -0700

    inital commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function(request, response){
+	response.writeHead(200, {'Content-Type': 'text/plain'} );
+	response.end('My first node app');
+}).listen(8080);
+console.log('Server is running at http://localhost:8080/');
